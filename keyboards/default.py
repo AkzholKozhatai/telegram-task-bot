c78d505803110c22f2046f9e723b3b9f916a526c
AkzholKozhatai <161103918+AkzholKozhatai@users.noreply.github.com>
2025-07-28 18:32:34 +0600

    Update default.py

--- keyboards/default.py
+++ keyboards/default.py
@@ -1,1 +1,11 @@
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+
+main_menu = ReplyKeyboardMarkup(
+    keyboard=[
+        [KeyboardButton(text="Отправить отчёт")],
+        [KeyboardButton(text="Моя статистика")],
+        [KeyboardButton(text="Помощь")]
+    ],
+    resize_keyboard=True
+)
 
