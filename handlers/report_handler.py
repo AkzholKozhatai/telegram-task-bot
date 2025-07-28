54f85d4a6771c07ba94a6e2eba6b30715e6d59a6
AkzholKozhatai <161103918+AkzholKozhatai@users.noreply.github.com>
2025-07-28 18:31:37 +0600

    Update report_handler.py

--- handlers/report_handler.py
+++ handlers/report_handler.py
@@ -1,1 +1,9 @@
+from aiogram import types
+from aiogram.dispatcher.filters import Command
+
+from loader import dp
+
+@dp.message_handler(Command("report"))
+async def send_report(message: types.Message):
+    await message.answer("Пожалуйста, введите текст вашего отчета.")
 
