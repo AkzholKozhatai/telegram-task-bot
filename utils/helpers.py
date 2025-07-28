fcff21610a7783f0309456e74535d9f7a2491ac8
AkzholKozhatai <161103918+AkzholKozhatai@users.noreply.github.com>
2025-07-28 18:32:51 +0600

    Update helpers.py

--- utils/helpers.py
+++ utils/helpers.py
@@ -1,1 +1,8 @@
+from datetime import datetime
+
+def get_current_datetime():
+    return datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+def is_admin(user_id: int, admin_ids: list[int]) -> bool:
+    return user_id in admin_ids
 
