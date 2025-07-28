7b511f7f0121d5415894a04e04fa5f2406f0b897
AkzholKozhatai <161103918+AkzholKozhatai@users.noreply.github.com>
2025-07-28 18:31:05 +0600

    Update start_handler.py

--- handlers/start_handler.py
+++ handlers/start_handler.py
@@ -1,1 +1,9 @@
+from aiogram import types
+from aiogram.dispatcher.filters import Command
+
+from loader import dp
+
+@dp.message_handler(Command("start"))
+async def start_command(message: types.Message):
+    await message.answer("Привет! Добро пожаловать в корпоративного бота.")
 
