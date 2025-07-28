22b15d852cc810f19d0bd2772d21d4fb860888c3
AkzholKozhatai <161103918+AkzholKozhatai@users.noreply.github.com>
2025-07-28 18:30:06 +0600

    Update database.py

--- db/database.py
+++ db/database.py
@@ -1,1 +1,12 @@
+import sqlite3
+
+def get_db_connection():
+    conn = sqlite3.connect('company.db')
+    return conn
+
+def init_db():
+    from db.models import create_tables
+    conn = get_db_connection()
+    create_tables(conn)
+    conn.close()
 
